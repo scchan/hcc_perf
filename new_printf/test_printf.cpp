ffcd0356d5d343faecc7899b73cbe87a3c1baedb
scchan <siuchi.chan@amd.com>
2016-03-23 00:30:12 -0500

    test printf 64bit signed integers

--- new_printf/test_printf.cpp
+++ new_printf/test_printf.cpp
@@ -10,11 +10,15 @@ int main() {
 
   const char* print_uint64 = "uint64: %lu\n";
   uint64_t u64_data = 1234;
+  const char* print_int64 = "int64: %ld\n";
+  uint64_t s64_data = 5678;
+
 
 
   hc::parallel_for_each(hc::extent<1>(64), [=](hc::index<1> idx) [[hc]] {
     if (idx[0]==0) {
       hc::printf(pb, print_uint64, u64_data);
+      hc::printf(pb, print_int64, s64_data);
     }
   }).wait();
 
