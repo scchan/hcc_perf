b10271f4f426aaebad0fe629592238a2cc5fcc92
scchan <siuchi.chan@amd.com>
2016-06-14 15:18:42 -0400

    update float2 example

--- float_2/test_float2.cpp
+++ float_2/test_float2.cpp
@@ -1,11 +1,19 @@
 #include <hc.hpp>
 #include <hc_short_vector.hpp>
 
-int main() {
+int main(int argc, char* argv[]) {
 
   hc::array_view<hc::short_vector::float2,1> data(64);
+  hc::short_vector::float2 hf2 = { 1000.0f, 2000.0f };
+  
+  if (argc >=3) {
+    hf2.x = atof(argv[1]);
+    hf2.y = atof(argv[2]);
+  }
+
   hc::parallel_for_each(data.get_extent(), [=](hc::index<1> i) [[hc]] {
     hc::short_vector::float2 v = { (float)i[0], 100.0f + i[0] };
+    v+=hf2;
     data[i] = v;
   });
 
