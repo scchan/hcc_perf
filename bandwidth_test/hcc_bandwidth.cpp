816d05437713dd7af334bbb01bf4bdf39041626c
scchan <siuchi.chan@amd.com>
2016-11-16 12:12:43 -0500

    hcc bandwidth

---
+++ bandwidth_test/hcc_bandwidth.cpp
@@ -0,0 +1,21 @@
+
+#include <hc.hpp>
+
+
+
+void array_write_bandwidth(hc::accelerator_view& av, const unsigned int size, const unsigned int iter) {
+
+
+
+}
+
+
+int main(int argc, char* argv[]) {
+
+
+  
+
+
+}
+
+
