9a74b4c7b52a2ae591819c588ef7c85665574111
Siu Chi Chan <siuchi.chan@amd.com>
2019-02-13 19:21:11 -0500

    hip example using dlopen

---
+++ dynamic_loading/sl0.cpp
@@ -0,0 +1,6 @@
+#include "dl_common.hpp"
+
+extern "C"
+bool run_add_int() {
+  return run_add<int>();
+}
---
+++ dynamic_loading/sl1.cpp
@@ -0,0 +1,6 @@
+#include "dl_common.hpp"
+
+extern "C"
+bool run_add_unsigned() {
+  return run_add<unsigned>();
+}
