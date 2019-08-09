d8581830478400ca5091463f02dc894e7e8e2e5b
Siu Chi Chan <siuchi.chan@amd.com>
2019-08-09 16:52:21 -0400

    update create_stream_priority

--- create_stream_priority/create_stream_priority.cpp
+++ create_stream_priority/create_stream_priority.cpp
@@ -13,15 +13,12 @@
 
 #define CHECK_HIP_ERROR(a) { if ((a) != HIP_SUCCESS) { abort(); } }
 
-__global__ void dummy() {
-    #if 0
-  *output = 0;
-  for (int i = 0; i < N; ++i) {
-    *output+=input[i];
+__global__ void dummy(int* output, int init_value, int multiplier, int num_iters) {
+  *output = init_value;
+  for (int i = 0; i < num_iters; ++i) {
+    *output+=*output*multiplier;
   }
-  #endif
 }
-
 
 void run(const int thread_id, const bool descending, const int num_streams_per_priority) {
 
@@ -40,32 +37,48 @@ void run(const int thread_id, const bool descending, const int num_streams_per_p
     end_priority = gp;
     bump = -1; 
   }
-  
+
+  struct gpu_resources {
+    hipStream_t s = 0;
+    int stream_priority;
+    int* output = nullptr;
+    gpu_resources(int stream_priority) : stream_priority(stream_priority) {
+      CHECK_HIP_ERROR(hipStreamCreateWithPriority(&s , hipStreamDefault, stream_priority));
+      CHECK_HIP_ERROR(hipMalloc(&output, sizeof(output))); 
+    }
+    gpu_resources(gpu_resources&& t) : 
+       s(t.s), stream_priority(t.stream_priority),
+       output(t.output) {
+      t.s = 0;
+      t.output = nullptr;
+    }
+    ~gpu_resources() {
+      if (s)      CHECK_HIP_ERROR(hipStreamDestroy(s));
+      if (output) CHECK_HIP_ERROR(hipFree(output));
+    }
+  };
+
+  std::vector<gpu_resources> v_gpu_resources;
   int num_streams = 0;
-  std::vector<hipStream_t> v_stream;
   for (int p = start_priority; ; p+=bump) {
       for (int np = 0; np < num_streams_per_priority; ++np, ++num_streams) {
           printf("Thread %d: creating stream %d with priority %d\n", thread_id, num_streams, p);
-          hipStream_t s;
-          CHECK_HIP_ERROR(hipStreamCreateWithPriority(&s , hipStreamDefault, p));
-          hipLaunchKernelGGL(dummy, dim3(1), dim3(1), 0, s);
-          v_stream.push_back(s);
+          gpu_resources r(p);
+          hipLaunchKernelGGL(dummy, dim3(1), dim3(1), 0, r.s, r.output, 0, 1, 1000);
+          v_gpu_resources.push_back(std::move(r));
       }
       if (p == end_priority)
         break;
-  } 
-
-  for (auto& s : v_stream) {
-      CHECK_HIP_ERROR(hipStreamSynchronize(s));
-      CHECK_HIP_ERROR(hipStreamDestroy(s));
   }
-
+  for (auto& r : v_gpu_resources) {
+    CHECK_HIP_ERROR(hipStreamSynchronize(r.s)); 
+  }
 }
 
 int main(int argc, char* argv[]) {
 
-  int num_threads = 16;
-  int num_streams_per_priority = 30;
+  int num_threads = 8;
+  int num_streams_per_priority = 24;
 
 #ifdef _GNU_SOURCE
   while (1) {
