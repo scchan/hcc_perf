b2861fd6f8232617d06732981e2ad07193aa94c7
Siu Chi Chan <siuchi.chan@amd.com>
2020-05-05 16:05:50 -0400

    update pass by value example

--- pass_by_value/pass_by_value.cpp
+++ pass_by_value/pass_by_value.cpp
@@ -35,6 +35,23 @@ __global__ void k(A<T, N> a, T* b, const int iter) {
 template __global__ void k(A<int, num_elements>, int*, const int);
 
 
+template<typename T, int N>
+__attribute((noinline))
+__device__ void k_const_aggregate_callee(const A<T, N> a, T* b, const int iter) {
+    b[0] = static_cast<T>(0);
+    int idx = blockIdx.x % N;
+    for (int i = 0; i < iter; ++i) {
+        b[0] += a.a[idx];
+        idx = (idx == N-1) ? 0 : idx + 1;
+    }
+}
+template<typename T, int N>
+__global__ void k_const_aggregate_caller(const A<T, N> a, T* b, const int iter) {
+    k_const_aggregate_callee(a, b, iter);
+}
+template __global__ void k_const_aggregate_caller(const A<int, num_elements>, int*, const int);
+
+
 #endif
 
 template<typename T, int N>
@@ -47,4 +64,16 @@ void host_k_const_aggregate(const A<T, N> a, T* b, const int blockidx, const int
     }
 }
 template void host_k_const_aggregate(const A<int, num_elements>, int*, const int, const int);
+
+
+template<typename T, int N>
+void host_k(A<T, N> a, T* b, const int blockidx, const int iter) {
+    b[0] = static_cast<T>(0);
+    int idx = blockidx;
+    for (int i = 0; i < iter; ++i) {
+        b[0] += a.a[idx];
+        idx = (idx == N-1) ? 0 : idx + 1;
+    }
+}
+template void host_k(A<int, num_elements>, int*, const int, const int);
 
