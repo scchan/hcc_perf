8fac4eb4da1ef2fd9130b506f77a8bc026d9fef5
Siu Chi Chan <siuchi.chan@amd.com>
2021-06-10 10:33:22 -0400

    update vf example with obj in LDS

--- virtual_function/private_shared_mem/private_shared_mem.cpp
+++ virtual_function/private_shared_mem/private_shared_mem.cpp
@@ -7,6 +7,11 @@ struct b {
   virtual void my_print() {
     printf("Hello\n");
   }
+  __host__ 
+  __device__
+  virtual int get() {
+    return 100;
+  }
 };
 
 struct d : b {
@@ -15,6 +20,11 @@ struct d : b {
   virtual void my_print() override {
     printf("World\n");
   }
+  __host__ 
+  __device__
+  virtual int get() override {
+    return 200;
+  }
 };
 
 __global__ void abcd() {
@@ -22,14 +32,24 @@ __global__ void abcd() {
     x.my_print();
 }
 
+__global__ void xyz_mod() {
+  __shared__ char b[1024];
+  new ((d*)b) d();
+  auto v = ((d*)b)->get();
+  printf("%s: get(): %d\n", __PRETTY_FUNCTION__, v);
+}
+
 __global__ void xyz() {
-  __shared__ char b[128];
+  __shared__ char b[1024];
   new ((d*)b) d();
   ((d*)b)->my_print();
 }
 
 int main() {
     abcd<<<1,1>>>();
+    hipDeviceSynchronize();
+    xyz_mod<<<1,1>>>();
+    hipDeviceSynchronize();
     xyz<<<1,1>>>();
     hipDeviceSynchronize();
     return 0;
