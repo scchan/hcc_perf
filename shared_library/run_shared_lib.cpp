1c9d3321772e31f87584eb54f840229899f8207e
Siu Chi Chan <siuchi.chan@amd.com>
2021-07-22 16:21:27 -0400

    add kernel to program's executable

--- shared_library/run_shared_lib.cpp
+++ shared_library/run_shared_lib.cpp
@@ -1,6 +1,13 @@
+#include "hip/hip_runtime.h"
 #include "A.h"
+
+__global__ void world() {
+   printf("World\n");
+}
 
 int main() {
    run_hello();
+   world<<<1,1>>>();
+   hipDeviceSynchronize();
    return 0;
 }
