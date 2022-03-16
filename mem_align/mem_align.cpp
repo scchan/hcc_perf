8d1be72ef2067f00c051a19570278248c679a093
Siu Chi Chan <siuchi.chan@amd.com>
2022-03-16 12:19:03 -0400

    update mem_align

--- mem_align/mem_align.cpp
+++ mem_align/mem_align.cpp
@@ -2,13 +2,22 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
+#include <vector>
 
 int main() {
- 
     std::cout << "__STDCPP_DEFAULT_NEW_ALIGNMENT__ = " << __STDCPP_DEFAULT_NEW_ALIGNMENT__ << std::endl;
 
-    auto i = new int{};
-    printf("alignof(i) = %lu\n", alignof(i));
-    delete i;
+    std::vector<int*> vi;
+    for (int i = 0; i < 16; ++i) {
+        vi.push_back(new int{});
+    }
+    
+    for(auto i : vi) {
+        auto j = reinterpret_cast<std::intptr_t>(i);
+        printf("j %% 8 = %lu\n", j%8);
+        printf("j %% 16 = %lu\n", j%16);
+        printf("j %% 32 = %lu\n", j%32);
+        delete i;
+    }
     return 0;
 }
